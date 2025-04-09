a35e1d33e0ea2319ce76e34bc5f50c09fc8fea58
IvanAXu <ivan@tangjt.cn>
2025-04-10 01:30:54 +0800

    013|U

--- main/013/run.sh
+++ main/013/run.sh
@@ -1,5 +1,14 @@
-tee /etc/docker/daemon.json <<-'EOF'
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://xxxxxx.mirror.aliyuncs.com"]
+        "registry-mirrors": [
+                "https://xxxxxx.mirror.aliyuncs.com", 
+                "https://reg-mirror.qiniu.com/",
+                "https://registry.docker-cn.com",
+                "https://hub-mirror.c.163.com",
+                "https://mirror.baidubce.com"
+        ]
 }
 EOF
+sudo systemctl daemon-reload
+
